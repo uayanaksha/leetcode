121f818b9429f7b247e92eb0843c675d2893830f
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2025-10-18 14:24:53 +0530

    !accept https://leetcode.com/problems/power-of-four/submissions/1804857836/

---
+++ 342/main.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n == 1) return true;
+        if(n < 4) return false;
+        return n%4 == 0 && isPowerOfFour(n/4);
+    }
+};
