ea99c185f9425c2717709e083a7a455a71b62b92
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2025-11-28 14:08:33 +0530

    !sub-2 https://leetcode.com/problems/longest-valid-parentheses/submissions/1841626809
    Runtime:  O(n)    0ms       (100%)
    Memory:   O(n)    12.58MB   (8.66%)

--- 32/main.cpp
+++ 32/main.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int longestValidParentheses(string s) {
         const int n = s.size();
+        int maxim{}, tmp{};
         vector<char> v;
         vector<int> idx;
         vector<bool> mem(s.size(), false);
-        int maxim{}, tmp{};
         for (int i{}; i < n; ++i) {
             char c = s[i];
             if (s[i] == ')' && !v.empty() && v.back() == '(') {
@@ -13,17 +13,13 @@ public:
                 v.pop_back();
                 idx.pop_back();
             } else {
-                idx.push_back(i);
                 v.push_back(c);
+                idx.push_back(i);
             }
         }
         for (bool e: mem) {
-            if (e){
-                maxim = max(maxim, ++tmp);
-            }
-            else {
-                tmp = 0;
-            }
+            if (e){ maxim = max(maxim, ++tmp); }
+            else { tmp = 0; }
         }
         return maxim;
     }
