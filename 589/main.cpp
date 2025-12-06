0e187a395e9d1102da3d395ddbf01b2628da987d
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2025-12-06 20:32:40 +0530

    !accept https://leetcode.com/problems/n-ary-tree-preorder-traversal/submissions/1848406198

---
+++ 589/main.cpp
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    using vi = vector<int>;
+    void _preorder(Node* root, vi& v){
+        if(!root) return;
+        v.push_back(root->val);
+        for(auto &child: root->children){
+            _preorder(child, v);
+        }
+    }
+    vector<int> preorder(Node* root) {
+        vi v;
+        _preorder(root, v);
+        return v;
+    }
+};
