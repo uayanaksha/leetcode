a141ced861ebc58617917bc719017a9874dfbe94
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2025-12-06 20:34:49 +0530

    !accept https://leetcode.com/problems/n-ary-tree-postorder-traversal/submissions/1848438842

---
+++ 590/main.cpp
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    using vi = vector<int>;
+    void _postorder(Node* root, vi& v){
+        if(!root) return;
+        for(auto &child: root->children){
+            _postorder(child, v);
+        }
+        v.push_back(root->val);
+    }
+    vector<int> postorder(Node* root) {
+        vi v;
+        _postorder(root, v);
+        return v;
+    }
+};
