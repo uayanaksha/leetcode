88ac49ff0b38eaede78d54ae9f5c1dfa784d4363
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2025-12-10 06:59:47 +0530

    !sub-2 https://leetcode.com/problems/count-the-number-of-computer-unlocking-permutations/submissions/1851564587

--- 3577/main.cpp
+++ 3577/main.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     int countPermutations(vector<int>& complexity) {
-        int minim=INT_MAX, min_ctr{}, n = complexity.size(), MOD = 1e9 + 7;
-        long r=1;
-        for(int& e: complexity){
-            if(e == minim){
-                ++min_ctr;
-            } else if(e < minim){
-                min_ctr = 1;
-                minim = e;
+        long n = static_cast<long>(complexity.size()), MOD = static_cast<long>(1e9 + 7), r = 1;
+        for(long i=1; i<n; ++i){
+            if(complexity[i] <= complexity[0]){
+                return 0;
             }
-        }
-        bool is_min_in_front = minim == complexity.front();
-        if(!is_min_in_front || min_ctr > 1) return 0;
-        for(int i=2; i<n; ++i){
             r = (r*i)%MOD;
         }
         return r;
