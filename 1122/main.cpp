f93afbd21f7f1d20c41f4e8781342e42edde0cb0
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2025-12-14 11:53:18 +0530

    !sub-2 https://leetcode.com/problems/relative-sort-array/submissions/1855130402
    Runtime:  O(N)    0ms       (100%)
    Memory:   O(N)    10.98MB   (84.68%)

--- 1122/main.cpp
+++ 1122/main.cpp
@@ -2,20 +2,26 @@ class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
         vector<int> mem(1001, 0);
+        int maxim=1000;
+        int minim=0;
+        int i{};
         for(int &n: arr1){
+            maxim = max(maxim, n);
+            minim = min(minim, n);
             mem[n]++;
         }
-        int i{};
         for(int &n: arr2){
             while(mem[n]--){
                 arr1[i++] = n;
             }
         }
-        for(int j{}; j<=1000; ++j){
-            if(mem[j] <= 0) continue;
-            while(mem[j]--){
-                arr1[i++] = j;
+        while(minim <= maxim){
+            if(mem[minim] > 0) {
+                while(mem[minim]--){
+                    arr1[i++] = minim;
+                }
             }
+            ++minim;
         }
         return arr1;
     }
