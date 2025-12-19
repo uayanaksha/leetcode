6092f15cd4bb80843792667811485a8cf41542ab
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2025-12-20 00:01:45 +0530

    !sub-2 https://leetcode.com/problems/find-all-people-with-secret/submissions/1860038140

--- 2092/main.cpp
+++ 2092/main.cpp
@@ -1,35 +1,36 @@
 class Solution {
+    template<typename T> using v=vector<T>;
+    int find(v<int>& mem, int val){
+        return mem[val] < 0 ? val : mem[val] = find(mem, mem[val]);
+    }
+    void unite(v<int>& mem, int x, int y){
+        x = find(mem, x);
+        y = find(mem, y);
+        if(x == y) return;
+        if(mem[x] > mem[y]) swap(x, y);
+        mem[x] += mem[y];
+        mem[y] = x;
+    }
 public:
-    template <typename T> using v = vector<T>;
-    template <typename T> using p = pair<T, T>;
-    vector<int> findAllPeople(int n, vector<vector<int>>& meetings,
-                              int firstPerson) {
-        v<v<pair<int, int>>> graph(n);
-        for (int i{}; i < meetings.size(); ++i) {
-            graph[meetings[i][0]].push_back({meetings[i][1], meetings[i][2]});
-            graph[meetings[i][1]].push_back({meetings[i][0], meetings[i][2]});
-        }
-        priority_queue<p<int>, v<p<int>>, greater<p<int>>> pq;
-        pq.push({0, 0});
-        pq.push({0, firstPerson});
-        v<int> r;
-        v<bool> visited(n, false);
-        while (!pq.empty()) {
-            p<int> curr = pq.top();
-            pq.pop();
-            if (visited[curr.second]){
-                continue;
+    v<int> findAllPeople(int n, v<v<int>>& meetings, int firstPerson) {
+        sort(meetings.begin(), meetings.end(), [](const auto& a, const auto& b){ 
+            return a[2] < b[2];
+        });
+        v<int> mem(n, -1), r;
+        unite(mem, 0, firstPerson);
+        for(int l{}, r{}; l<meetings.size();){
+            for(; r < meetings.size() && meetings[l][2] == meetings[r][2]; ++r){
+                unite(mem, meetings[r][0], meetings[r][1]);
             }
-            visited[curr.second] = true;
-            for (p<int> neighbour : graph[curr.second]) {
-                if (!visited[neighbour.first] && curr.first <= neighbour.second) {
-                    pq.push({neighbour.second, neighbour.first});
-                }
+            for(int s=find(mem, 0); l<r; ++l){
+                int x = meetings[l][0];
+                int y = meetings[l][1];
+                if(find(mem, x) != s) mem[x] = -1;
+                if(find(mem, y) != s) mem[y] = -1;
             }
         }
-        for (int i{}; i < n; ++i) {
-            if (visited[i])
-                r.push_back(i);
+        for(int i{}, s = find(mem, 0); i<n; ++i){
+            if(find(mem, i) == s) r.push_back(i);
         }
         return r;
     }
