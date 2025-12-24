482f6db2128198bee1333fd187e0236729e355ae
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2025-12-24 21:21:02 +0530

    !sub-2 https://leetcode.com/problems/apple-redistribution-into-boxes/submissions/1864307681
    Runtime:  O(N)  0ms       (100%)
    Memory:   O(1)  33.10MB   (77.58%)

--- 3074/main.cpp
+++ 3074/main.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
     int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
-        int total_apples{}, minim_boxes{};
-        sort(capacity.begin(), capacity.end(), greater<int>());
-        for(auto &v: apple){ 
-            total_apples += v; 
+        int total_apples{}, total_capacity{}, ctr{};
+        vector<int> mem(51, 0);
+        for (auto& v : apple) {
+            total_apples += v;
         }
-        for(auto &v: capacity){ 
-            total_apples -= v;
-            ++minim_boxes;
-            if(total_apples <= 0){
-                return minim_boxes;
+        for (auto& v : capacity) {
+            mem[v]++;
+        }
+        for (int i = 50; i >= 0 && total_capacity < total_apples; --i) {
+            if(mem[i] == 0) {
+                continue;
             }
+            int val = mem[i]*i;
+            if(total_capacity + val <= total_apples){
+                total_capacity = total_capacity + val;
+                ctr = ctr + mem[i];
+                mem[i] = 0;
+            } else {
+                total_capacity = total_capacity + i;
+                ctr = ctr + 1;
+                mem[i] = mem[i] - 1;
+            }
+            ++i;
         }
-        return capacity.size();
+        return ctr;
     }
 };
