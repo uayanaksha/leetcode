10d8ba3c5fc9ffa6117b82d8abd6457960d53ea0
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2026-02-26 00:47:33 +0530

    !sub-2 https://leetcode.com/problems/minimum-absolute-difference/submissions/1931126693
    Runtime:  14ms      77.17%
    Memory:   36.60MB   89.19%

--- 1200/main.cpp
+++ 1200/main.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
         sort(arr.begin(), arr.end());
-        int minim_diff = arr[1] - arr[0];
-        for(int i=1; i<arr.size(); ++i){
-            minim_diff = min(minim_diff, arr[i] - arr[i-1]);
-        }
         vector<vector<int>> v;
-        for(int i=1; i<arr.size(); ++i){
-            if(arr[i] - minim_diff == arr[i-1]){
-                v.push_back(vector<int>{arr[i-1], arr[i]});
+        int min_diff = 1 << 30;
+        for (int i = 1; i < arr.size(); ++i) {
+            int diff = arr[i] - arr[i - 1];
+            if (diff > min_diff)
+                continue;
+            if (diff < min_diff) {
+                v = {};
+                min_diff = diff;
             }
+            v.push_back(vector<int>{arr[i - 1], arr[i]});
         }
         return v;
     }
