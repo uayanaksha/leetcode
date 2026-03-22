69099f847f69b0e60a6868b473186ba030f90785
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2026-03-22 22:30:04 +0530

    !accept https://leetcode.com/problems/allow-one-function-call/submissions/1955954286

---
+++ 2666/main.ts
@@ -0,0 +1,21 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    let is_called = false;
+    return function (...args) {
+        if(is_called) {
+            return undefined;
+        }
+        is_called = true;
+        return fn(...args);
+    };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
