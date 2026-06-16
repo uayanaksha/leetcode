c80824f34180346f6a4ef92210fb8bc5fbd043bf
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2026-06-17 04:19:02 +0530

    !accept-2 https://leetcode.com/problems/combinations/submissions/2035684423

--- 77/main.cpp
+++ 77/main.cpp
@@ -1,23 +1,22 @@
 class Solution {
-public:
-    vector<int> getCombination(int n){
-        vector<int> v;
-        for(int i=1; n; ++i){
-            if (n & 1){
-                v.push_back(i);
-            }
-            n /= 2;
+    template<typename T>
+    using v = vector<T>;
+
+    void fn(int x, int n, int k, v<int>&& combination, v<v<int>>& result){
+        if(k == 0){
+            result.push_back(combination);
+            return;
         }
-        return v;
+        for(int i=x; i<=n; ++i){
+            combination.push_back(i);
+            fn(i+1, n, k-1, std::move(combination), result);
+            combination.pop_back();
+        }
     }
+public:
     vector<vector<int>> combine(int n, int k) {
-        int ub = 1 << n;
-        vector<vector<int>> v;
-        for(int i{}; i<ub; ++i){
-            if(__builtin_popcount(i) != k) continue;
-            vector<int> combination = getCombination(i);
-            v.push_back(combination);
-        }
-        return v;
+        v<v<int>> result;
+        fn(1, n, k, vector<int>(), result);
+        return result;
     }
 };
