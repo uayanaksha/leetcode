5d6669600c8d478fed5930a7784f2d83cd14611d
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2026-06-30 03:48:22 +0530

    !accept-3 https://leetcode.com/problems/rotate-list/submissions/2050547254

--- 61/main.cpp
+++ 61/main.cpp
@@ -1,3 +1,4 @@
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -8,6 +9,47 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+class Solution {
+public:
+    int getLength(ListNode* head){
+        if(!head) return 0;
+        return 1 + getLength(head->next);
+    }
+
+    ListNode* fn(ListNode* head, ListNode* next){
+        if(!head) {
+            return next;
+        }
+        head->next = fn(head->next, next);
+        return head;
+    }
+
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head || !head->next) return head;
+        int n = getLength(head);
+        k = k % n;
+        if(k == 0) return head;
+        ListNode *ptr = head;
+        for(int i=1; i+k<n; ++i){
+            ptr = ptr->next;
+        }
+        ListNode* next = ptr->next;
+        ptr->next = nullptr;
+        ListNode* res = fn(next, head);
+        return res;
+    }
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
@@ -33,3 +75,4 @@ public:
         return head;
     }
 };
+*/
