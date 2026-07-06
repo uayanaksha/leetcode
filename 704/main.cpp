da6c43257297577c7d627f2870249cae63d4b817
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2026-07-06 09:59:38 +0530

    !accept https://leetcode.com/problems/binary-search/submissions/2057523226

---
+++ 704/main.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+        int mid;
+        while(l<=r){
+            mid = l + (r-l)/2;
+            if(nums[mid] == target){
+                return mid;
+            } else if(target < nums[mid]){
+                r = mid-1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
