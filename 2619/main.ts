b34341b3fc24f96a3481eec5f01005087374a73a
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2026-08-16 18:54:12 +0530

    !accept https://leetcode.com/problems/array-prototype-last/submissions/2109017065

---
+++ 2619/main.ts
@@ -0,0 +1,13 @@
+interface Array<T> {
+    last(): T | -1;
+}
+
+Array.prototype.last = function() {
+    return (this.length > 0) ? this.at(this.length - 1) : -1;
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
+
