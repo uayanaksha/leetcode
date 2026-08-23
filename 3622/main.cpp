bbf8f1cf067dad6bc1798b907c6df6fed8c64d8b
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2026-08-24 01:06:33 +0530

    !accept https://leetcode.com/problems/check-divisibility-by-digit-sum-and-product/submissions/2117772161

---
+++ 3622/main.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+        int sum = 0;
+        int product = 1;
+        int x = n;
+        while(x > 0){
+            sum += (x % 10);
+            product *= (x % 10);
+            x /= 10;
+        }
+        return n % (sum + product) == 0;
+    }
+};
