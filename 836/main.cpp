d55c27d1dcf91b70302ca2752105f1bc0d8f2238
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2026-09-18 23:04:59 +0530

    !accept https://leetcode.com/problems/rectangle-overlap/submissions/2145995403

---
+++ 836/main.cpp
@@ -0,0 +1,6 @@
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rect1, vector<int>& rect2) {
+        return rect1[0] < rect2[2] && rect1[2] > rect2[0] && rect1[1] < rect2[3] && rect1[3] > rect2[1];
+    }
+};
